63859fd24310d3469e41d83d512e25e88b9be4b7
Kaspar Laineste <laineste@cock.li>
2020-06-19 23:42:40 +0300

    Total rewrite

--- finalsensor.py
+++ finalsensor.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 from time import sleep,time
+import configparser
 
 class linuxMontior(object):
 	"""docstring for linuxMontior"""
@@ -10,7 +11,8 @@ class linuxMontior(object):
 	def getTemps(self):
 		temps = {}
 		for tn,td in psutil.sensors_temperatures().items():
-			pass #TODO: implement dis shit
+			for to in td:
+				temps["local:%s:%s"%(tn,to.label)] = {"temp":to.current}
 		return temps
 class wmiMonitor(object):
 	"""docstring for wmiMonitor"""
@@ -23,15 +25,17 @@ class wmiMonitor(object):
 		temps = {}
 		for s in ohw:
 			if s.SensorType==u'Temperature':
-				temps[s.Identifier] = {"temp":s.Value}
+				temps["local:%s"%s.Identifier] = {"temp":s.Value}
 		return temps
+
 class iloMonitor(object):
 	"""docstring for iloMonitor"""
-	def __init__(self, username, password):
+	def __init__(self, username, password, addr):
 		super(iloMonitor, self).__init__()
 		self.sess = r.Session()
 		self.loginSucc = False
-		loginresp = self.sess.post("%s/json/login_session"%ip,data=j.dumps({"method":"login","user_login":username,"password":password}),verify=False)
+		self.iloAddr = addr
+		loginresp = self.sess.post("%s/json/login_session"%self.iloAddr,data=j.dumps({"method":"login","user_login":username,"password":password}),verify=False)
 		if "session_key" in loginresp.json().keys():
 			self.session_key = loginresp.json()["session_key"]
 			print("Login success!")
@@ -40,7 +44,7 @@ class iloMonitor(object):
 			print("Login fail!")
 
 	def logout(self):
-		self.sess.post("%s/json/login_session"%ip,data=j.dumps({"method":"logout","session_key":self.session_key}),verify=False)
+		self.sess.post("%s/json/login_session"%self.iloAddr,data=j.dumps({"method":"logout","session_key":self.session_key}),verify=False)
 		self.logged = False
 
 	def getTemps(self):
@@ -48,35 +52,17 @@ class iloMonitor(object):
 			print("Not logged in!")
 			return
 		temps = {}
-		for x in self.sess.get("%s/json/health_temperature?_=%i"%(ip,int(time())*1000),verify=False).json()["temperature"]:
-			temps[x["label"]] = {"temp":x["currentreading"],"caution":x["caution"],"critical":x["critical"]}
+		for x in self.sess.get("%s/json/health_temperature?_=%i"%(self.iloAddr,int(time())*1000),verify=False).json()["temperature"]:
+			temps["ilo%s"%x["label"]] = {"temp":x["currentreading"],"caution":x["caution"],"critical":x["critical"]}
 		return temps
 
-if os.name=="nt":
-	print("We are running on windows, importing wmi")
-	import wmi
-	temps = wmiMonitor()
-else:
-	print("We are running on Linux, importing psutil :)")
-	import psutil
-	temps = None
-
-try:
-	import serial
-except:
-	print("pyserial not installed!")
-	exit()
-
-
-ip = "https://10.31.31.3"
-serialport = "COM3"
 
 class serialFan(object):
 	"""docstring for serialFan"""
 	def __init__(self, serport=False):
 		super(serialFan, self).__init__()
 		self.fans = [15,15,15,15,15,15]
-		self.ser  = serial.Serial(serport,9600,timeout=1)
+		self.ser = serial.Serial(serport,9600,timeout=1) if serport else False
 
 	def setFan(self,fan,speed):
 		self.fans[fan-1] = speed
@@ -85,128 +71,124 @@ class serialFan(object):
 			self.ser.write(b"s\n")
 			self.ser.write(b"%i\n"%fan)
 			self.ser.write(b"%i\n"%fpwm)
+		else:
+			print("Setting fan %i to %i%%"%(fan,speed))
 
 
 	def getFan(self,fan):
 		return self.fans[fan-1]		
 
-parser = argparse.ArgumentParser(description="Control HP fans over serial via custom controller")
-parser.add_argument('--ilouser', help="HP Ilo username")
-parser.add_argument('--ilopass', help="HP Ilo username")
-parser.add_argument('--mode',choices=["smart","simple"], default="smart", help="Simple: all fans same rpm")
-parser.add_argument('--listsensors',action="store_true", help="List available sensors")
-parser.add_argument('--static',type=int, default=-1, help="Set all fans to this speed")
-args = parser.parse_args()
-if args.ilouser and args.ilopass:
-	print("importing ILO requirements...")
-	import requests as r
-	import json as j
-	temps = iloMonitor(args.ilouser,args.ilopass)
-if args.listsensors:
-	print("Listing all sensors")
-	for sn,st in temps.getTemps().items():
-		print("%s - %iC"%(sn,st["temp"]))
-	exit()
 
-
-tempCurves = {
-	"CPU": {
-		50:10,
-		60:20,
-		65:25,
-		70:30,	
-		80:50
-	},
-	"GPU": {
-		60:10,
-		70:20,
-		80:25,
-		95:35
-	},
-	"HDD":{
-		40:10,
-		50:20,
-		60:30
-	}
-}
-sensors = {
-	"/intelcpu/0/temperature/4":{"fans":[4,5,6],"curve":"CPU"},
-	"/intelcpu/1/temperature/4":{"fans":[1,2,3],"curve":"CPU"},
-	"/atigpu/0/temperature/0":{"fans":[4,5],"curve":"GPU"},
-	"/hdd/0/temperature/0":{"fans":[1,2],"curve":"HDD"},
-	"/hdd/1/temperature/0":{"fans":[1,2],"curve":"HDD"},
-}
-
-def curveFind(temp,curve):
-	cs = 0
-	for trigTemp,fanSpeed in tempCurves[curve].items():
-		if trigTemp < temp:
-			cs = fanSpeed
-		else:
-			return cs
-	return cs
-
-fans = {
-	1:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-	2:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-	3:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-	4:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-	5:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-	6:{"lastSpeed":0,"tsChange":0,"tempAtTs":0,"nextSpeed":0},
-}
-
-waitDelay = 3
-termalDelay = 5
-sf = serialFan(serialport)
-if args.static >= 0:
+if os.name=="nt":
 	try:
-		while True:
-			for x in range(6):
-				sf.setFan(x,args.static)
-	except KeyboardInterrupt:
-		print("exiting")
-	exit()
-if args.mode=="smart":
-	print("Staring in smart mode")
-
-
-
-	t = temps.getTemps()
-	for tn,td in t.items():
-		try:
-			sensor = sensors[tn]
-			for fanNum in sensor["fans"]:
-				fans[fanNum]["lastSpeed"] = max(fans[fanNum]["lastSpeed"],curveFind(td["temp"],sensor["curve"]))
-		except KeyError:
-			pass
-	while True:
-		lstart = time()
-		t = temps.getTemps()
-		for tn,td in t.items():
-			try:
-				sensor = sensors[tn]
-				for fanNum in sensor["fans"]:
-					fans[fanNum]["nextSpeed"] = max(fans[fanNum]["nextSpeed"],curveFind(td["temp"],sensor["curve"]))
-			except KeyError:
-				pass
-		for fanNum in fans.keys():
-			if fans[fanNum]["nextSpeed"] != fans[fanNum]["lastSpeed"]:
-				if fans[fanNum]["tsChange"] == 0:
-					fans[fanNum]["tsChange"] = time()
-					fans[fanNum]["tempAtTs"] = td["temp"]
-				else:
-					if fans[fanNum]["tsChange"]+waitDelay<time() or (fans[fanNum]["tempAtTs"] - td["temp"] < -termalDelay and fans[fanNum]["nextSpeed"] < fans[fanNum]["lastSpeed"]):
-						fans[fanNum]["lastSpeed"] = fans[fanNum]["nextSpeed"]
-						fans[fanNum]["tsChange"] = 0
-						fans[fanNum]["tempAtTs"] = 0
-			else:
-				fans[fanNum]["tsChange"] = 0
-				fans[fanNum]["tempAtTs"] = 0
-		for fanNum,fanData in fans.items():
-			sf.setFan(fanNum,fanData["lastSpeed"])
-			fans[fanNum]["nextSpeed"] = 0
-		print([[x["lastSpeed"],x["lastSpeed"],x["tempAtTs"]] for x in fans.values()])
-		sleep(max(1-(time()-lstart),0))
-
+		import wmi
+	except ModuleNotFoundError:
+		print("wmi not installed!")
+		exit()
+	temps = wmiMonitor()
 else:
-	print("Starting in simple mode")
+	try:
+		import psutil
+	except ModuleNotFoundError:
+		print("'psutil' not installed!")
+		exit()
+	temps = linuxMontior()
+
+try:
+	import serial
+except ModuleNotFoundError:
+	print("pyserial not installed!")
+	exit()
+
+parser = argparse.ArgumentParser(description="Control HP fans over serial via custom controller")
+parser.add_argument('--config', help="Load config", required=True)
+parser.add_argument('--listsensors',action="store_true", help="List available sensors")
+parser.add_argument('--debug',action="store_true", help="Enable debug mode (only for developing and testing)")
+parser.add_argument('--setall',type=int, default=-1, help="Set all fans to this speed")
+args = parser.parse_args()
+
+config = configparser.ConfigParser()
+config.read(args.config)
+
+if args.debug:
+	serPort = False
+else:
+	serPort = config["General"]["serialPort"]
+
+ilo = None
+if config.getboolean("General","useILO"):
+	ilo = iloMonitor(config["ILO"]["username"],config["ILO"]["password"],config["ILO"]["address"])
+
+if args.listsensors:
+	for tn,tv in temps.getTemps().items():
+		print("'%s' : %.1f"%(tn,tv["temp"]))
+	if ilo:
+		print("Ilo configured, will now show all ILO sensors:")
+		for tn,tv in ilo.getTemps().items():
+			print("'%s' : %.1f"%(tn,tv["temp"]))
+	exit()
+
+sf = serialFan(serPort)
+
+if args.setall != -1:
+	if 0 > args.setall > 100:
+		print("Fan speed has to be between 0-100")
+		exit()
+	for x in range(1,7):
+		sf.setFan(x,args.setall)
+		print("Set channel %i to %i%% speed"%(x,args.setall))
+	exit()
+
+
+def calcSpeed(temp,curve):
+	largestfan = 0
+	largesttemp = 0
+	for ctemp,cfan in curve.items():
+		if temp<ctemp: break
+		largestfan = cfan
+		largesttemp = temp
+	return (largestfan,largesttemp)
+
+#now the fun begins
+wait = int(config["General"]["updateInterval"])
+
+profiles = {}
+
+for p in config["General"]["profiles"].split(","):
+	try:
+		hys = int(config[p]["hysteresis"])
+	except KeyError:
+		hys = int(config["General"]["defaultHysteresis"])
+	curve = {}
+	for ct,cf in dict(config[config[p]["curve"]]).items(): curve[int(ct)] = int(cf)
+	profiles[p] = {
+		"fans":[int(x) for x in config[p]["fans"].split(",")],
+		"sensors":config[p]["sensors"].split(","),
+		"curve":curve,
+		"hysteresis":hys,
+		"lowerTemp":0,
+		"lastSpeed":0
+	}
+
+while True:
+	maxFanSpeeds = {1:0,2:0,3:0,4:0,5:0,6:0,}
+	curTemps = temps.getTemps()
+	if ilo:
+		curTemps = curTemps+ilo.getTemps()
+	for pn,pd in profiles.items():
+		maxtemp = max([curTemps[x]["temp"] for x in pd["sensors"]])
+		spd,temp = calcSpeed(maxtemp,pd["curve"])
+		if maxtemp < pd["lowerTemp"]:#temp has lowered under hysterisis
+			profiles[pn]["lowerTemp"] = temp-pd["hysteresis"]
+			profiles[pn]["lastSpeed"] = spd
+		elif pd["lastSpeed"] < spd:#temp has risen over next point
+				profiles[pn]["lowerTemp"] = temp-pd["hysteresis"]
+				profiles[pn]["lastSpeed"] = spd
+		for x in pd["fans"]:
+			maxFanSpeeds[x] = max(maxFanSpeeds[x],profiles[pn]["lastSpeed"])
+	for fn,fs in maxFanSpeeds.items():
+		print(maxFanSpeeds)
+		#sf.setFan(fn,fs)
+	sleep(wait)
+
+
